2c57d3d0d3e03676dc6af80186b4648657c97a87
morten <morten@tranebaerkaeret.dk>
2016-08-26 07:56:39 +0200

    Changes to pure pdfjs

--- client/components/docctrl/docctrl.js
+++ client/components/docctrl/docctrl.js
@@ -1,29 +1,50 @@
 angular.module('leukemiapp').controller('docctrl', docctrl);
 
+
+
 function docctrl($scope, $stateParams) {
 
     console.log("DocCtrl "+ JSON.stringify($stateParams));
 
-        $scope.pdfName = $stateParams.url;
-        $scope.pdfUrl = "/pdf/"+$stateParams.url+".pdf";
-        $scope.scroll = 0;
-        $scope.loading = 'loading';
+    $scope.pdfName = $stateParams.url;
+    $scope.pdfUrl = "/pdf/"+$stateParams.url+".pdf";
+    $scope.canvas = document.getElementById('pdfdocument');
+    $scope.ctx = $scope.canvas.getContext('2d');
 
-        $scope.getNavStyle = function(scroll) {
-            if(scroll > 100) return 'pdf-controls fixed';
-            else return 'pdf-controls';
-        }
+    $scope.renderPage = function(num) {
 
-        $scope.onError = function(error) {
-            console.log(error);
-        }
+        // Update page number
+        $scope.pageNum = num;
 
-        $scope.onLoad = function() {
-            $scope.loading = '';
-        }
+        // Using promise to fetch the page
+        pdfDoc.getPage($scope.pageNum).then(function(page) {
+            var viewport = page.getViewport($scope.canvas.width / page.getViewport(1.0).width);
+            $scope.canvas.height = viewport.height;
+            // Render PDF page into canvas context
+            var renderContext = {
+                canvasContext: ctx,
+                viewport: viewport
+            };
+            page.render(renderContext);
+        });
+    }
 
-        $scope.onProgress = function(progress) {
-            console.log(progress);
-        }
+    $scope.goPrevious = function() {
+        if (pageNum <= 1) return;
+        pageNum--;
+        $scope.renderPage(pageNum);
+    }
+
+    $scope.goNext = function() {
+        if (pageNum >= pdfDoc.numPages) return;
+        pageNum++;
+        $scope.renderPage(pageNum);
+    }
+
+    PDFJS.getDocument($scope.pdfUrl).then(function getPdfHelloWorld(pdfDoc) {
+        $scope.pdfDoc = pdfDoc;
+        $scope.pageCount = pdfDoc.numPages;
+        $scope.renderPage(pageNum);
+    });
 };
 
