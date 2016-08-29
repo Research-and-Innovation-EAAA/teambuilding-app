2928fc1fdd0c02df65f8082c9625debb6aad10a0
morten <morten@tranebaerkaeret.dk>
2016-08-29 20:10:09 +0200

    Setup native app submission criterea

--- mobile-config.js
+++ mobile-config.js
@@ -1,1 +1,10 @@
 App.accessRule('*');
+App.info({
+    id: 'dk.sundata.leukemia.www',
+    version: '1.0.0',
+    name: 'HowRyou',
+    description: 'Register decease symptoms and have your doctor ensure the best treatment continously',
+    author: 'Morten Mathiasen',
+    email: 'morten@webconsultservice.dk',
+    website: 'http://infomotor.dk',
+});
