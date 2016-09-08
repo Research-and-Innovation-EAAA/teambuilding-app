ea71241ae26638ae1a51601a0ddbe6efc2c62235
Jakub <jkb@eaaa.dk>
2016-09-08 10:08:23 +0200

    single question mandatory (on/off), disable graph button when no data for it

--- client/components/wizard/templates/single-question.controller.js
+++ client/components/wizard/templates/single-question.controller.js
@@ -1,57 +1,67 @@
 angular.module('leukemiapp').controller('singleQuestionController', singleQuestionController);
 
 function singleQuestionController($scope, $reactive, WizardHandler, $ionicScrollDelegate) {
-   $reactive(this).attach($scope);
-   var vm = this;
+    $reactive(this).attach($scope);
+    var vm = this;
 
-   $ionicScrollDelegate.$getByHandle('wizardStepContent').freezeScroll(false);
+    $ionicScrollDelegate.$getByHandle('wizardStepContent').freezeScroll(false);
 
-   var dataType = Session.get('registrationType');
-   var stepNumber = WizardHandler.wizard().currentStepNumber();
+    var dataType = Session.get('registrationType');
+    var stepNumber = WizardHandler.wizard().currentStepNumber();
 
-   var step = {};
-   var config = {};
+    var step = {};
+    var config = {};
 
-   for (i = 0; i < Modules.length; i++) {
-      if (Modules[i].name === dataType) {
+    for (i = 0; i < Modules.length; i++) {
+        if (Modules[i].name === dataType) {
 
-         if (Modules[i].wizard.steps[stepNumber - 2] !== undefined) {
-            step = Modules[i].wizard.steps[stepNumber - 2];
+            if (Modules[i].wizard.steps[stepNumber - 2] !== undefined) {
+                step = Modules[i].wizard.steps[stepNumber - 2];
 
-            if (step.stepTemplate.config !== undefined) {
-               config = step.stepTemplate.config;
+                if (step.stepTemplate.config !== undefined) {
+                    config = step.stepTemplate.config;
+                } else {
+                    console.error('No config defined for module ', dataType, ' step number ', stepNumber);
+                }
             } else {
-               console.error('No config defined for module ', dataType, ' step number ', stepNumber);
+                console.error('Step undefined for module ', dataType, ' step number ', stepNumber);
             }
-         } else {
-            console.error('Step undefined for module ', dataType, ' step number ', stepNumber);
-         }
-         break;
-      }
-   }
+            break;
+        }
+    }
 
-   vm.registration = Session.get('registration');
+    vm.registration = Session.get('registration');
 
-   console.log("config is " , config);
-   vm.question = config.question;
-   vm.answers = config.answers;
-   vm.propertyName = config.propertyName;
+    console.log("config is ", config);
+    vm.question = config.question;
+    vm.answers = config.answers;
+    vm.mandatory = config.mandatory;
+    vm.propertyName = config.propertyName;
 
-   vm.selectAnswer = (answer) => {
-      vm.registration[config.propertyName] = answer;
-      updateSessionVariables();
-   };
+    vm.selectAnswer = (answer) => {
+        vm.registration[config.propertyName] = answer;
+        updateSessionVariables();
+    };
 
-   function updateSessionVariables() {
-      var validated = Session.get('regValidated');
-      if (validated === undefined)
-         validated = [];
+    vm.validation = (registration) => {
+        if (vm.mandatory) {
+            return registration[config.propertyName] != null;
+        }
+        else return true;
+    }
 
-      validated[stepNumber - 1] = step.validation(vm.registration);
-      Session.set('regValidated', validated);
-      console.log('regValidated session variable updated: ', validated);
+    function updateSessionVariables() {
+        var validated = Session.get('regValidated');
+        if (validated === undefined)
+            validated = [];
 
-      Session.set('registration', vm.registration);
-      console.log('Registration updated: ', vm.registration);
-   }
+        validated[stepNumber - 1] = vm.validation(vm.registration);
+        Session.set('regValidated', validated);
+        console.log('regValidated session variable updated: ', validated);
+
+        Session.set('registration', vm.registration);
+        console.log('Registration updated: ', vm.registration);
+    }
+
+    updateSessionVariables();
 }
