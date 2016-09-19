934c4b36b06c73be1167b987613a7177fd794a74
Jakub <jkb@eaaa.dk>
2016-09-19 13:33:50 +0200

    Overlapping title fixed #7

--- client/components/frontpage/frontpage.controller.js
+++ client/components/frontpage/frontpage.controller.js
@@ -1,6 +1,6 @@
 angular.module('leukemiapp').controller('frontpageController', FrontpageController);
 
-function FrontpageController($scope, $rootScope, $reactive, $ionicModal, ModuleManagementService, $timeout) {
+function FrontpageController($scope, $rootScope, $reactive, $ionicModal, $ionicNavBarDelegate, ModuleManagementService, $timeout) {
     $reactive(this).attach($scope);
     var vm = this;
 
@@ -11,6 +11,14 @@ function FrontpageController($scope, $rootScope, $reactive, $ionicModal, ModuleM
 
     Accounts.ui.config({
         passwordSignupFields: "USERNAME_ONLY"
+    });
+
+    $scope.$on('$ionicView.enter', function()
+    {
+        $timeout(function()
+        {
+            $ionicNavBarDelegate.align('center');
+        });
     });
 
     //Analytics
