46515e372ff349f6ae8eccf878d6caaa7e0abde4
Jakub <jkb@eaaa.dk>
2017-03-30 11:29:22 +0200

    frontpage styling, layout

--- client/routes.js
+++ client/routes.js
@@ -1,58 +1,59 @@
 angular.module('leukemiapp')
-    .config(function ($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
+    .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 
-       // Ionic uses AngularUI Router which uses the concept of states
-       // Learn more here: https://github.com/angular-ui/ui-router
-       $stateProvider
-           .state('app', {
-              url: '/app',
-              templateUrl: 'client/components/side-menu/side-menu.html'
-           })
-           .state('app.login', {
-               url: '/login',
-               views: {
-                   'menuContent': {
-                       templateUrl: 'client/components/login/login.html'
-                   }
-               }
-           })
-           .state('app.eventselect', {
-               url: '/eventselect',
-               views: {
-                   'menuContent': {
-                       templateUrl: 'client/components/event-select/event-select.html'
-                   }
-               }
-           })
-           .state('app.event', {
-              url: '/event',
-              views: {
-                 'menuContent': {
-                    templateUrl: 'client/components/event/event.html'
-                 }
-              }
-           })
-           .state('app.docctrl', {
-              url: '/docctrl/:url',
-              views: {
-                 'menuContent': {
-                    templateUrl: 'client/components/docctrl/docctrl.html'
-                 }
-              }
-           })
-           .state('app.questionwizard', {
-              url: '/questionwizard',
-              views: {
-                 'menuContent': {
-                    templateUrl: 'client/components/wizard/question-wizard.html'
-                 }
-              }
-           });
-
-       // if none of the above states are matched, use this as the fallback
-       $urlRouterProvider.otherwise('/app/login');
-
-
-       // general settings
+        // general settings
         $ionicConfigProvider.backButton.previousTitleText(true);
+
+
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        $stateProvider
+            .state('app', {
+                url: '/app',
+                templateUrl: 'client/components/side-menu/side-menu.html'
+            })
+            .state('app.login', {
+                url: '/login',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'client/components/login/login.html'
+                    }
+                }
+            })
+            .state('app.eventselect', {
+                url: '/eventselect',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'client/components/event-select/event-select.html'
+                    }
+                }
+            })
+            .state('app.event', {
+                url: '/event',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'client/components/event/event.html'
+                    }
+                }
+            })
+            .state('app.docctrl', {
+                url: '/docctrl/:url',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'client/components/docctrl/docctrl.html'
+                    }
+                }
+            })
+            .state('app.questionwizard', {
+                url: '/questionwizard',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'client/components/wizard/question-wizard.html'
+                    }
+                }
+            });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/login');
+
     });
