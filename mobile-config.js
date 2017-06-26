6232ee5354c0e02e70aaca8c215f4d565089f878
Jakub <jkb@eaaa.dk>
2017-06-26 12:10:01 +0200

    mobile config

--- mobile-config.js
+++ mobile-config.js
@@ -1,9 +1,9 @@
 App.accessRule('*');
 App.info({
-    id: 'dk.sundata.leukemia.www',
+    id: 'dk.eaaa.teambuilding',
     version: '1.0.0',
-    name: 'HowRyou',
-    description: 'Register decease symptoms and have your doctor ensure the best treatment continously',
+    name: 'Teambuilding',
+    description: 'App for gathering feedback for teambuilding activities.',
     author: 'Morten Mathiasen',
     email: 'morten@webconsultservice.dk',
     website: 'http://infomotor.dk',
